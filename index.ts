acaf3a75d40afd69ed130a6d24d8de697d45095c
Amitrajeet Sharma <amitrajeets450@gmail.com>
2026-06-12 16:08:35 +0000

    Commander Cli

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+import { Command } from "commander";
+const program = new Command();
+
+program
+    .name("ShellScript")
+    .description("Ai based command line interface")
+    .version("0.0.1")
+
+program
+    .command("WakeUp")
+    .description("Show the Banner and pick cli or telegram mode")
+    .action(
+        async()=>{
+            console.log("wakeup calling");
+        }
+    );
+await program.parseAsync(process.argv);
